214e7f7ce1f6151fef1d59802bb241277e8c33c6
haakensonb <untamedhawk44@gmail.com>
2022-12-29 15:40:56 -0700

    Solve day_06, part_2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -12,22 +12,37 @@ fn has_duplicate(input: Vec<char>) -> bool {
     return false;
 }
 
-fn part_1(input: &str) -> Result<usize, &str> {
+fn detect_marker(input: &str, group_size: usize) -> Result<usize, &str> {
     let input_chars: Vec<char> = input.chars().collect();
-    for i in 3..input_chars.len() {
-        let last_4: Vec<char> = input[i-3..=i].chars().collect();
-        if !has_duplicate(last_4) {
+    let group_offset = group_size - 1;
+    for i in group_offset..input_chars.len() {
+        let group: Vec<char> = input[i-group_offset..=i].chars().collect();
+        if !has_duplicate(group) {
             return Ok(i + 1);
         }
     }
     return Err("Could not find valid marker in input!");
 }
 
+fn part_1(input: &str) -> usize {
+    let result = detect_marker(input, 4);
+    return result.unwrap();
+}
+
+fn part_2(input: &str) -> usize {
+    let result = detect_marker(input, 14);
+    return result.unwrap();
+}
+
+
 fn main() {
     let data: String = read_to_string("input.txt").expect("Could not read file!");
     
     let answer_1 = part_1(&data);
-    println!("Part 1: {}", answer_1.unwrap());
+    println!("Part 1: {}", answer_1);
+
+    let answer_2 = part_2(&data);
+    println!("Part 2: {}", answer_2);
 }
 
 #[cfg(test)]
@@ -48,26 +63,31 @@ mod tests {
 
     #[test]
     fn test_part_1_ex_1(){
-        assert_eq!(part_1("mjqjpqmgbljsphdztnvjfqwrcgsmlb").unwrap(), 7);
+        assert_eq!(part_1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
     }
 
     #[test]
     fn test_part_1_ex_2(){
-        assert_eq!(part_1("bvwbjplbgvbhsrlpgdmjqwftvncz").unwrap(), 5);
+        assert_eq!(part_1("bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
     }
 
     #[test]
     fn test_part_1_ex_3(){
-        assert_eq!(part_1("nppdvjthqldpwncqszvftbrmjlhg").unwrap(), 6);
+        assert_eq!(part_1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
     }
 
     #[test]
     fn test_part_1_ex_4(){
-        assert_eq!(part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg").unwrap(), 10);
+        assert_eq!(part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
     }
 
     #[test]
     fn test_part_1_ex_5(){
-        assert_eq!(part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw").unwrap(), 11);
+        assert_eq!(part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+
+    #[test]
+    fn test_part_2_ex_1(){
+        assert_eq!(part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
     }
 }
